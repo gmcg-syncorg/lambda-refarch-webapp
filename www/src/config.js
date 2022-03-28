ab7f7132fc8da37915730dca3d4b5640c4822a21
Gary McGilvary <gary.mcgilvary@syncorg.com>
2022-03-28 13:36:09 +0100

    Frontend config update

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "7vf59is28lvidvb9fi0hsdukgf",     // CognitoClientID
-  "api_base_url": "https://9bboptc5ed.execute-api.eu-west-2.amazonaws.com/prod",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "mytodoappdemo-syncorg-platform-arch.auth.eu-west-2.amazoncognito.com",                   // CognitoDomainName
-  "redirect_url": "https://master.d1oxpqe1febune.amplifyapp.com"                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "None",     // CognitoClientID
+  "api_base_url": "None",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "None",                   // CognitoDomainName
+  "redirect_url": "None"                                      // AmplifyURL
 };
 
 export default config;
